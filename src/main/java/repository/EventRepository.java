ffbdb1aebb1f97ea48a9cd69a388bfe3b98c8732
ryuzera <ryugfx@gmail.com>
2025-05-22 16:50:26 -0300

    created Repository and Service

---
+++ src/main/java/repository/EventRepository.java
@@ -0,0 +1,8 @@
+package repository;
+
+import model.Event;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EventRepository extends CrudRepository<Event, Integer> {
+    public Event findByPrettyName(String prettyName);
+}
---
+++ src/main/java/service/EventService.java
@@ -0,0 +1,29 @@
+package service;
+
+import model.Event;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.EventRepository;
+
+import java.util.List;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository repository;
+
+    public Event addNewEvent(Event event) {
+        //gerando o pretty name
+        event.setPrettyName(event.getTitle().toLowerCase().replaceAll(" ", "-"));
+        return repository.save(event);
+    }
+
+    public List<Event> getAllEvents() {
+        return (List<Event>) repository.findAll();
+    }
+
+    public Event getByPrettyName(String prettyName) {
+        return repository.findByPrettyName(prettyName);
+    }
+}
